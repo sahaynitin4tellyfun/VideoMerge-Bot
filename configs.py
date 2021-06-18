918e6a98dc73635570cfcfc42ba66af09db0f25b
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-06-18 09:51:26 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -20,12 +20,12 @@ class Config(object):
     BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
 
     START_TEXT = """
-Hi Unkil, I am Video Merge Bot!
-I can Merge Multiple Videos in One Video. Video Formats should be same.
+**Hii, I'm a Simple Video Merge Bot!**
+**I can Merge Multiple Videos into One Video. Video Formats should be same for all the videos.**
 
-Made by @AbirHasan2005
+**Made by @AniMesH941**
 """
-    CAPTION = "Video Merged by @{}\n\nMade by @AbirHasan2005"
+    CAPTION = "**Video Merged by @{}**\n\n**Made by @AniMesH941**"
     PROGRESS = """
 Percentage : {0}%
 Done: {1}
