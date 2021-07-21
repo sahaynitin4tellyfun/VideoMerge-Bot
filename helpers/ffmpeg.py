1c8ed2453e3d933d1a9d6eb93f291a7fe1f23a8f
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-21 23:30:36 +0530

    Update ffmpeg.py

--- helpers/ffmpeg.py
+++ helpers/ffmpeg.py
@@ -16,9 +16,9 @@ async def MergeVideo(input_file: str, user_id: int, message: Message, format_: s
     :param format_: Pass File Extension.
     :return: This will return Merged Video File Path
     """
-  if Message.document.file_name is not None:
-    output_vid = f"{Config.DOWN_PATH}/{str(user_id)}/[@AniMesH941]_Merged.{format_.lower()}"
-    file_generator_command = [
+    if Message.document.file_name is not None:
+        output_vid = f"{Config.DOWN_PATH}/{str(user_id)}/[@AniMesH941]_Merged.{format_.lower()}"
+        file_generator_command = [
         "ffmpeg",
         "-f",
         "concat",
