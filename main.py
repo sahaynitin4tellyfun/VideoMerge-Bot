81e3f4369f05bcf2f9450854794f7d7f249c66fd
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-21 15:21:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -337,8 +337,8 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
             QueueDB.update({cb.from_user.id: []})
             FormtDB.update({cb.from_user.id: None})
             return
-        vid_list = list()
-        await cb.message.edit(f"**Successfully Merged the videos! \n\nTotal Videos Merged : {vid_list} 👀**")
+        vid_list = []
+        await cb.message.edit(f"**Successfully Merged the videos! \n\nTotal Videos Merged : {len(vid_list)} 👀**")
         await asyncio.sleep(Config.TIME_GAP)
         file_size = os.path.getsize(merged_vid_path)
         if int(file_size) > 2097152000:
