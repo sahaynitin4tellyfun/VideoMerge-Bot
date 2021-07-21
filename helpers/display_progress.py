956d55f96f6424da8901ff5063682eb943a6836b
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-07-22 00:51:53 +0530

    Update display_progress.py

--- helpers/display_progress.py
+++ helpers/display_progress.py
@@ -30,11 +30,8 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         )
         try:
             await message.edit(
-                text="**{}**\n\n {}".format(
-                    ud_type,
-                    tmp
-                ),
-                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⛔ Cancel Process 🗑️", callback_data="cancelProcess")]]))
+                text="**{}**\n\n {}".format(ud_type, tmp),
+                reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("⛔ Cancel Process 🗑️", callback_data="cancelProcess")]]),
                 parse_mode='markdown'
             )
         except:
