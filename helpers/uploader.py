830428d8c16542ae4b254d58c009d258f8465e2c
sahaynitin4tellyfun <87252200+sahaynitin4tellyfun@users.noreply.github.com>
2021-08-09 14:14:26 +0530

    Update uploader.py

--- helpers/uploader.py
+++ helpers/uploader.py
@@ -30,7 +30,7 @@ async def UploadVideo(bot: Client, cb: CallbackQuery, merged_vid_path: str, widt
                 ),
                 reply_markup=InlineKeyboardMarkup(
                     [[
-                         InlineKeyboardButton("👀 More Amazing Botz 🤖", url="https://t.me/tellybots_4u")]
+                         InlineKeyboardButton("👀 More Amazing Botz 🤖", url="https://t.me/dkbotz")]
                     ]
                 )
             )
@@ -49,7 +49,7 @@ async def UploadVideo(bot: Client, cb: CallbackQuery, merged_vid_path: str, widt
                 ),
                 reply_markup=InlineKeyboardMarkup(
                     [[
-                         InlineKeyboardButton("👀 More Amazing Botz 🤖", url="https://t.me/tellybots_4u")]
+                         InlineKeyboardButton("👀 More Amazing Botz 🤖", url="https://t.me/dkbotz")]
                     ]
                 )
             )
